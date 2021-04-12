0192768701ec0181dae7a1e4e2def31a6645bb7d
Daniel McKnight <daniel@neongecko.com>
2021-04-12 16:41:07 -0700

    Neon Audio Module initial implementation

---
+++ setup.py
@@ -0,0 +1,56 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+#
+# Copyright 2008-2021 Neongecko.com Inc. | All Rights Reserved
+#
+# Notice of License - Duplicating this Notice of License near the start of any file containing
+# a derivative of this software is a condition of license for this software.
+# Friendly Licensing:
+# No charge, open source royalty free use of the Neon AI software source and object is offered for
+# educational users, noncommercial enthusiasts, Public Benefit Corporations (and LLCs) and
+# Social Purpose Corporations (and LLCs). Developers can contact developers@neon.ai
+# For commercial licensing, distribution of derivative works or redistribution please contact licenses@neon.ai
+# Distributed on an "AS IS” basis without warranties or conditions of any kind, either express or implied.
+# Trademarks of Neongecko: Neon AI(TM), Neon Assist (TM), Neon Communicator(TM), Klat(TM)
+# Authors: Guy Daniels, Daniel McKnight, Regina Bloomstine, Elon Gasper, Richard Leeds
+#
+# Specialized conversational reconveyance options from Conversation Processing Intelligence Corp.
+# US Patents 2008-2021: US7424516, US20140161250, US20140177813, US8638908, US8068604, US8553852, US10530923, US10530924
+# China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("./version.py", "r", encoding="utf-8") as v:
+    for line in v.readlines():
+        if line.startswith("__version__"):
+            if '"' in line:
+                version = line.split('"')[1]
+            else:
+                version = line.split("'")[1]
+
+with open("./requirements.txt", "r", encoding="utf-8") as r:
+    requirements = r.readlines()
+
+setup(
+    name='neon-audio',
+    version=version,
+    description=long_description,
+    url='https://github.com/NeonGeckoCom/neon_audio',
+    author='Neongecko',
+    author_email='developers@neon.ai',
+    license='NeonAI License v1.0',
+    packages=['neon_core_server'],
+    install_requires=requirements,
+    zip_safe=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': [
+            'neon_audio_client=neon_audio.__main__:main'
+        ]
+    }
+)
