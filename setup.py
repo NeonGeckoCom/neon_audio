8c5a4ba54479aa325d160b7c7cb85b3953598ac8
Daniel McKnight <daniel@neongecko.com>
2021-05-26 12:47:31 -0700

    Update requirements to reference holmesV Update requirements parsing in setup.py

--- setup.py
+++ setup.py
@@ -18,6 +18,22 @@
 # China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
 
 from setuptools import setup, find_packages
+from os import getenv, path
+
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.lower().strip() for r in requirements if r.lower().strip() and not r.strip().startswith("#")]
+    print(requirements)
+    if getenv("GITHUB_TOKEN"):
+        for i in range(0, len(requirements)):
+            r = requirements[i]
+            if "github.com" in r:
+                requirements[i] = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
+    return requirements
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -30,9 +46,6 @@ with open("./version.py", "r", encoding="utf-8") as v:
             else:
                 version = line.split("'")[1]
 
-with open("./requirements.txt", "r", encoding="utf-8") as r:
-    requirements = r.readlines()
-
 setup(
     name='neon-audio',
     version=version,
@@ -42,7 +55,7 @@ setup(
     author_email='developers@neon.ai',
     license='NeonAI License v1.0',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=get_requirements("requirements.txt"),
     zip_safe=True,
     classifiers=[
         'Intended Audience :: Developers',
